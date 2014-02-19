d7b6c173c1d2ae10606b08416d28d6b3a26881fb
Stephanie Sharp <stephrsharp@gmail.com>
2014-02-19 20:00:12 +1100

    Accepting Edits

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -1,9 +1,18 @@
 Todos.TodoController = Ember.ObjectController.extend({
   actions: {
-     editTodo: function() {
-       this.set('isEditing', true);
-     }
-   },
+    editTodo: function() {
+      this.set('isEditing', true);
+    },
+    acceptChanges: function() {
+      this.set('isEditing', false);
+
+      if (Ember.isEmpty(this.get('model.title'))) {
+        this.send('removeTodo');
+      } else {
+        this.get('model').save();
+      }
+    }
+  },
 
   isEditing: false,
 
